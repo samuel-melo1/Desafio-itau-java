39ccd030a75f8395175531247f81d6c04d742e7a
samuel-melo1 <samuelprogramacao3@gmail.com>
2025-03-26 23:11:59 -0300

    initial commit

---
+++ src/main/java/com/samuelm/desafioitau/domain/Transacao.java
@@ -0,0 +1,29 @@
+package com.samuelm.desafioitau.domain;
+
+import java.time.OffsetDateTime;
+public class Transacao {
+
+    private Double valor;
+    private OffsetDateTime dataHora;
+
+    public Transacao(Double valor, OffsetDateTime dataHora) {
+        this.valor = valor;
+        this.dataHora = dataHora;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public OffsetDateTime getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(OffsetDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+}
